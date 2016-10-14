b98d26674968e2b4f87ac684716bcc2a123781c0
Adam Woodbeck <awoodbeck@barracuda.com>
2016-10-14 17:18:37 +0100

    Export MultipleClaim fields

--- registry.go
+++ registry.go
@@ -90,8 +90,8 @@ var ErrCantGloballyRegister = errors.New("can't globally register this address")
 // and that the "current" situation (to the extent that is definable)
 // may change at any time.
 type MultipleClaim struct {
-	claimants []Address
-	name      string
+	Claimants []Address
+	Name      string
 }
 
 type stopRegistry struct{}
